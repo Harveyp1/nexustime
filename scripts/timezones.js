e6505e99efa713e890273a615d28d2f880a7f299
Harveyp1 <110130940+Harveyp1@users.noreply.github.com>
2025-07-05 11:11:44 -0500

    Add files via upload

---
+++ scripts/timezones.js
@@ -0,0 +1,29 @@
+const timezones = [
+  { value: "Pacific/Midway", label: "(UTC-11:00) Midway Island, Samoa" },
+  { value: "Pacific/Honolulu", label: "(UTC-10:00) Hawaii" },
+  { value: "America/Anchorage", label: "(UTC-09:00) Alaska" },
+  { value: "America/Los_Angeles", label: "(UTC-08:00) Pacific Time (US & Canada)" },
+  { value: "America/Denver", label: "(UTC-07:00) Mountain Time (US & Canada)" },
+  { value: "America/Chicago", label: "(UTC-06:00) Central Time (US & Canada)" },
+  { value: "America/New_York", label: "(UTC-05:00) Eastern Time (US & Canada)" },
+  { value: "America/Caracas", label: "(UTC-04:00) Caracas" },
+  { value: "America/St_Johns", label: "(UTC-03:30) Newfoundland" },
+  { value: "America/Argentina/Buenos_Aires", label: "(UTC-03:00) Buenos Aires" },
+  { value: "Atlantic/Azores", label: "(UTC-01:00) Azores" },
+  { value: "UTC", label: "(UTC+00:00) Coordinated Universal Time" },
+  { value: "Europe/London", label: "(UTC+00:00) London" },
+  { value: "Europe/Berlin", label: "(UTC+01:00) Berlin, Paris, Rome" },
+  { value: "Europe/Moscow", label: "(UTC+03:00) Moscow" },
+  { value: "Asia/Dubai", label: "(UTC+04:00) Abu Dhabi, Muscat" },
+  { value: "Asia/Karachi", label: "(UTC+05:00) Karachi" },
+  { value: "Asia/Kolkata", label: "(UTC+05:30) India Standard Time" },
+  { value: "Asia/Dhaka", label: "(UTC+06:00) Dhaka" },
+  { value: "Asia/Jakarta", label: "(UTC+07:00) Jakarta, Bangkok" },
+  { value: "Asia/Shanghai", label: "(UTC+08:00) Beijing, Shanghai" },
+  { value: "Asia/Tokyo", label: "(UTC+09:00) Tokyo, Osaka" },
+  { value: "Australia/Adelaide", label: "(UTC+09:30) Adelaide" },
+  { value: "Australia/Sydney", label: "(UTC+10:00) Sydney, Melbourne" },
+  { value: "Pacific/Auckland", label: "(UTC+12:00) Auckland, Wellington" }
+];
+
+export default timezones;
